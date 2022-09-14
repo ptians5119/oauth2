207c2d331b6c1474673675ac2d0d8b3c199961d6
pengte <406388783@qq.com>
2022-09-14 20:51:47 +0800

    fix

--- oxide-auth-db/src/db_service/scylla.rs
+++ oxide-auth-db/src/db_service/scylla.rs
@@ -10,8 +10,6 @@ pub(crate) struct ScyllaHandler {
     handle: thread::JoinHandle<()>,
     input: mpsc::Sender<String>,
     output: Arc<mpsc::Receiver<StringfiedEncodedClient>>,
-    db_name: String,
-    db_table: String,
 }
 
 impl ScyllaHandler {
@@ -25,7 +23,7 @@ impl ScyllaHandler {
                 .build().unwrap();
 
             let _ = rt.block_on(async {
-                let session = get_session(&db_nodes, db_user.as_str(), db_pwd.as_str()).await.map_err(|e|
+                let session = ScyllaHandler::get_session(&db_nodes, db_user.as_str(), db_pwd.as_str()).await.map_err(|e|
                     Error::new(ErrorKind::Other, format!("{:?}", e)))?;
                 let session = Arc::new(session);
                 let rx = Arc::new(rx1);
@@ -35,16 +33,16 @@ impl ScyllaHandler {
                             if msg.eq("stop") {
                                 break
                             } else {
-                                let client = get_app(
+                                let client = ScyllaHandler::get_app_by_db(
                                     session.clone(),
-                                    db_name.as_str(),
-                                    db_table.as_str(),
+                                    db_name.clone().as_str(),
+                                    db_table.clone().as_str(),
                                     msg.as_str())
                                     .await.map_err(|err| Error::new(ErrorKind::Other, err.to_string()))?;
                                 tx2.send(client).unwrap();
                             }
                         }
-                        Err(err) => Err(Error::new(ErrorKind::Other, err.to_string()))
+                        Err(err) => return Err(Error::new(ErrorKind::Other, err.to_string()))
                     }
                 }
                 Ok(())
@@ -54,8 +52,6 @@ impl ScyllaHandler {
             handle: th,
             input: tx1,
             output: Arc::new(rx2),
-            db_name,
-            db_table
         }
     }
 
@@ -69,47 +65,47 @@ impl ScyllaHandler {
     {
         let _ = self.input.send("stop".to_string());
     }
+
+    async fn get_session(db_nodes: &Vec<String>, db_user: &str, db_pwd: &str) -> Result<Session, NewSessionError>
+    {
+        SessionBuilder::new()
+            .known_nodes(&db_nodes)
+            .user(db_user, db_pwd)
+            .load_balancing(Arc::new(RoundRobinPolicy::new()))
+            .build()
+            .await
+    }
+
+    async fn get_app_by_db(session: Arc<Session>, db_name: &str, db_table: &str, client_id: &str) -> Result<StringfiedEncodedClient, Error>
+    {
+        let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+                    , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, db_table, client_id);
+        let res = {
+            match session.query(smt.clone(), &[]).await {
+                Ok(r) => {
+                    r
+                },
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
+                }
+            }
+        };
+        for row in res.rows.unwrap()
+            .into_typed::<StringfiedEncodedClient>() {
+            let client = match row {
+                Ok(r) => r,
+                Err(_e) => {
+                    return Err(Error::new(ErrorKind::Other, "match row error"))
+                }
+            };
+            return Ok(client)
+        }
+        Err(Error::new(ErrorKind::NotFound, "no rows"))
+    }
 }
 
 impl Drop for ScyllaHandler {
     fn drop(&mut self) {
         let _ = self.handle.join();
     }
-}
-
-async fn get_session(db_nodes: &Vec<String>, db_user: &str, db_pwd: &str) -> Result<Session, NewSessionError>
-{
-    SessionBuilder::new()
-        .known_nodes(&db_nodes)
-        .user(db_user, db_pwd)
-        .load_balancing(Arc::new(RoundRobinPolicy::new()))
-        .build()
-        .await
-}
-
-async fn get_app(session: Arc<Session>, db_name: &str, db_table: &str, client_id: &str) -> Result<StringfiedEncodedClient, Error>
-{
-    let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
-                    , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, db_table, client_id);
-    let res = {
-        match session.query(smt.clone(), &[]).await {
-            Ok(r) => {
-                r
-            },
-            Err(e) => {
-                return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
-            }
-        }
-    };
-    for row in res.rows.unwrap()
-        .into_typed::<StringfiedEncodedClient>() {
-        let client = match row {
-            Ok(r) => r,
-            Err(_e) => {
-                return Err(Error::new(ErrorKind::Other, "match row error"))
-            }
-        };
-        return Ok(client)
-    }
-    Err(Error::new(ErrorKind::NotFound, "no rows"))
 }

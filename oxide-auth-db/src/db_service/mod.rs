c61d50af63bcff9bd082bc54f724a346c9b021d4
pengte <406388783@qq.com>
2022-09-15 13:57:43 +0800

    ..

--- oxide-auth-db/src/db_service/mod.rs
+++ oxide-auth-db/src/db_service/mod.rs
@@ -64,12 +64,15 @@ pub fn get_client(session: Arc<Mutex<Session>>, db_name: String, table_name: Str
         });
     });
     th.join().unwrap();
-    let rx = Arc::new(rx);
-    for _i in 0..3 {
-        debug!("input try_recv");
-        if let Ok(c) = rx.try_recv() {
-            return c
-        }
+    if let Ok(c) = rx.try_recv() {
+        return c
     }
+    // let rx = Arc::new(rx);
+    // for _i in 0..3 {
+    //     debug!("input try_recv");
+    //     if let Ok(c) = rx.try_recv() {
+    //         return c
+    //     }
+    // }
     Err(Error::new(ErrorKind::NotFound, "try recv error"))
 }

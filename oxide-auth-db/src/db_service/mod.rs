5414a1b09e6f911413515e982b682045f05afe5f
peter.peng <peter.peng@dsky.com>
2022-09-22 21:46:22 +0800

    try tokio current

--- oxide-auth-db/src/db_service/mod.rs
+++ oxide-auth-db/src/db_service/mod.rs
@@ -8,9 +8,12 @@ mod redis_cluster_scylla_cluster;
 use client_data::*;
 use std::io::{Error, ErrorKind};
 use scylla::{Session, IntoTypedRows};
-use std::sync::Arc;
+use std::sync::{Arc, mpsc};
 use tokio::sync::Mutex;
 use futures::executor::block_on;
+use tokio::runtime::Handle;
+use std::thread;
+use std::time::Duration;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "redis-isolate")] {
@@ -32,31 +35,68 @@ cfg_if::cfg_if! {
 }
 
 pub fn get_client(session: Arc<Mutex<Session>>, db_name: String, table_name: String, id: String) -> Result<StringfiedEncodedClient, Error> {
-    block_on(async {
-        let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+    // block_on(async {
+    //     let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+    //                 , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
+    //     let res = {
+    //         println!("inside");
+    //         let ss = session.lock().await;
+    //         println!("get session");
+    //         match ss.query(smt.clone(), &[]).await {
+    //             Ok(r) => r,
+    //             Err(e) => {
+    //                 return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
+    //             }
+    //         }
+    //     };
+    //     println!("get rows");
+    //     for row in res.rows.unwrap()
+    //         .into_typed::<StringfiedEncodedClient>() {
+    //         let client = match row {
+    //             Ok(r) => r,
+    //             Err(_e) => {
+    //                 return Err(Error::new(ErrorKind::Other, "parse client error"))
+    //             }
+    //         };
+    //         return Ok(client)
+    //     }
+    //     Err(Error::new(ErrorKind::Other, "no client"))
+    // })
+
+
+    let handle = Handle::current();
+    let (tx, rx) = mpsc::channel();
+    let th = thread::spawn(move || {
+        handle.spawn(async move {
+            let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
                     , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
-        let res = {
-            println!("inside");
-            let ss = session.lock().await;
-            println!("get session");
-            match ss.query(smt.clone(), &[]).await {
+            let res = match session.lock().await.query(smt.clone(), &[]).await {
                 Ok(r) => r,
                 Err(e) => {
-                    return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
-                }
-            }
-        };
-        println!("get rows");
-        for row in res.rows.unwrap()
-            .into_typed::<StringfiedEncodedClient>() {
-            let client = match row {
-                Ok(r) => r,
-                Err(_e) => {
-                    return Err(Error::new(ErrorKind::Other, "parse client error"))
+                    tx.send(Err(Error::new(ErrorKind::Other, format!("{:?}", e)))).unwrap();
+                    return
                 }
             };
-            return Ok(client)
-        }
-        Err(Error::new(ErrorKind::Other, "no client"))
-    })
+            for row in res.rows.unwrap()
+                .into_typed::<StringfiedEncodedClient>() {
+                let client = match row {
+                    Ok(r) => r,
+                    Err(_e) => {
+                        tx.send(Err(Error::new(ErrorKind::Other, "xxx2"))).unwrap();
+                        return
+                    }
+                };
+                debug!("get client");
+                tx.send(Ok(client)).unwrap();
+                return
+            }
+            tx.send(Err(Error::new(ErrorKind::NotFound, "no rows"))).unwrap();
+        });
+    });
+    th.join().unwrap();
+    let client = match rx.recv_timeout(Duration::from_millis(500)) {
+        Ok(c) => c,
+        Err(err) => Err(Error::new(ErrorKind::NotFound, err.to_string()))
+    };
+    client
 }

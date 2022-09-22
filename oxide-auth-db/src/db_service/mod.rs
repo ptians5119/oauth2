11f5ec9537abf0e7c0c520199c9efe797b47f9c2
peter.peng <peter.peng@dsky.com>
2022-09-22 17:06:06 +0800

    change the oxide-auth-actix's Message

--- oxide-auth-db/src/db_service/mod.rs
+++ oxide-auth-db/src/db_service/mod.rs
@@ -34,65 +34,65 @@ cfg_if::cfg_if! {
 }
 
 pub fn get_client(session: Arc<Mutex<Session>>, db_name: String, table_name: String, id: String) -> Result<StringfiedEncodedClient, Error> {
-    // futures::executor::block_on(async {
-    //     println!("input block_on");
-    //     let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
-    //                 , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
-    //     let res = {
-    //         let ss = session.lock().await;
-    //         match ss.query(smt.clone(), &[]).await {
-    //             Ok(r) => r,
-    //             Err(e) => {
-    //                 return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
-    //             }
-    //         }
-    //     };
-    //     for row in res.rows.unwrap()
-    //         .into_typed::<StringfiedEncodedClient>() {
-    //         let client = match row {
-    //             Ok(r) => r,
-    //             Err(_e) => {
-    //                 return Err(Error::new(ErrorKind::Other, "parse client error"))
-    //             }
-    //         };
-    //         debug!("get client");
-    //         return Ok(client)
-    //     }
-    //     Err(Error::new(ErrorKind::Other, "no client"))
-    // })
-    let (tx, rx) = oneshot::channel();
-    println!("oxide-db: inside");
-    System::current().arbiter().spawn(async move {
+    futures::executor::block_on(async {
+        println!("input block_on");
         let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
-                , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
-        let res = match session.lock().await.query(smt.clone(), &[]).await {
-            Ok(r) => r,
-            Err(e) => {
-                tx.send(Err(Error::new(ErrorKind::Other, format!("{:?}", e)))).unwrap();
-                return
+                    , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
+        let res = {
+            let ss = session.lock().await;
+            match ss.query(smt.clone(), &[]).await {
+                Ok(r) => r,
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
+                }
             }
         };
-        println!("oxide-db: get row");
         for row in res.rows.unwrap()
             .into_typed::<StringfiedEncodedClient>() {
             let client = match row {
                 Ok(r) => r,
                 Err(_e) => {
-                    tx.send(Err(Error::new(ErrorKind::Other, "xxx2"))).unwrap();
-                    return
+                    return Err(Error::new(ErrorKind::Other, "parse client error"))
                 }
             };
             debug!("get client");
-            tx.send(Ok(client)).unwrap();
-            return
+            return Ok(client)
         }
-        tx.send(Err(Error::new(ErrorKind::NotFound, "no rows"))).unwrap();
-    });
-    let client = match block_on(async {
-        rx.await
-    }) {
-        Ok(c) => c,
-        Err(err) => Err(Error::new(ErrorKind::NotFound, err.to_string()))
-    };
-    client
+        Err(Error::new(ErrorKind::Other, "no client"))
+    })
+    // let (tx, rx) = oneshot::channel();
+    // println!("oxide-db: inside");
+    // System::current().arbiter().spawn(async move {
+    //     let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+    //             , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
+    //     let res = match session.lock().await.query(smt.clone(), &[]).await {
+    //         Ok(r) => r,
+    //         Err(e) => {
+    //             tx.send(Err(Error::new(ErrorKind::Other, format!("{:?}", e)))).unwrap();
+    //             return
+    //         }
+    //     };
+    //     println!("oxide-db: get row");
+    //     for row in res.rows.unwrap()
+    //         .into_typed::<StringfiedEncodedClient>() {
+    //         let client = match row {
+    //             Ok(r) => r,
+    //             Err(_e) => {
+    //                 tx.send(Err(Error::new(ErrorKind::Other, "xxx2"))).unwrap();
+    //                 return
+    //             }
+    //         };
+    //         debug!("get client");
+    //         tx.send(Ok(client)).unwrap();
+    //         return
+    //     }
+    //     tx.send(Err(Error::new(ErrorKind::NotFound, "no rows"))).unwrap();
+    // });
+    // let client = match block_on(async {
+    //     rx.await
+    // }) {
+    //     Ok(c) => c,
+    //     Err(err) => Err(Error::new(ErrorKind::NotFound, err.to_string()))
+    // };
+    // client
 }

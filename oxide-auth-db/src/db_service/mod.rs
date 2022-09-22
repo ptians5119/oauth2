5fd487cf6d6fb38c933861cd7874b6af7c8c03ca
peter.peng <peter.peng@dsky.com>
2022-09-22 11:01:39 +0800

    use futures block_on

--- oxide-auth-db/src/db_service/mod.rs
+++ oxide-auth-db/src/db_service/mod.rs
@@ -34,41 +34,70 @@ cfg_if::cfg_if! {
 }
 
 pub fn get_client(session: Arc<Mutex<Session>>, db_name: String, table_name: String, id: String) -> Result<StringfiedEncodedClient, Error> {
-    let handle = Handle::current();
-    let (tx, rx) = mpsc::channel();
-    let th = thread::spawn(move || {
-        handle.spawn(async move {
-            let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+    futures::executor::block_on(async {
+        println!("input block_on");
+        let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
                     , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
-            let res = match session.lock().await.query(smt.clone(), &[]).await {
+        {
+            let ss = session.lock().await;
+            println!("get session");
+            let res = match ss.query(smt.clone(), &[]).await {
                 Ok(r) => r,
                 Err(e) => {
-                    tx.send(Err(Error::new(ErrorKind::Other, format!("{:?}", e)))).unwrap();
-                    return
+                    return Err(Error::new(ErrorKind::Other, format!("{:?}", e)))
                 }
             };
-            for row in res.rows.unwrap()
-                .into_typed::<StringfiedEncodedClient>() {
-                let client = match row {
-                    Ok(r) => r,
-                    Err(_e) => {
-                        tx.send(Err(Error::new(ErrorKind::Other, "xxx2"))).unwrap();
-                        return
-                    }
-                };
-                debug!("get client");
-                tx.send(Ok(client)).unwrap();
-                return
-            }
-            tx.send(Err(Error::new(ErrorKind::NotFound, "no rows"))).unwrap();
-        });
-    });
-    th.join().unwrap();
-    let client = match rx.recv_timeout(Duration::from_millis(100)) {
-        Ok(c) => c,
-        Err(err) => Err(Error::new(ErrorKind::NotFound, err.to_string()))
-    };
-    client
+        }
+        println!("get row");
+        for row in res.rows.unwrap()
+            .into_typed::<StringfiedEncodedClient>() {
+            let client = match row {
+                Ok(r) => r,
+                Err(_e) => {
+                    return Err(Error::new(ErrorKind::Other, "parse client error"))
+                }
+            };
+            debug!("get client");
+            return Ok(client)
+        }
+        Err(Error::new(ErrorKind::Other, "no client"))
+    })
+
+    // let handle = Handle::current();
+    // let (tx, rx) = mpsc::channel();
+    // let th = thread::spawn(move || {
+    //     handle.spawn(async move {
+    //         let smt = format!("SELECT client_id, client_secret, redirect_uri, additional_redirect_uris
+    //                 , scopes as default_scope FROM {}.{} where client_id = '{}'", db_name, table_name, id);
+    //         let res = match session.lock().await.query(smt.clone(), &[]).await {
+    //             Ok(r) => r,
+    //             Err(e) => {
+    //                 tx.send(Err(Error::new(ErrorKind::Other, format!("{:?}", e)))).unwrap();
+    //                 return
+    //             }
+    //         };
+    //         for row in res.rows.unwrap()
+    //             .into_typed::<StringfiedEncodedClient>() {
+    //             let client = match row {
+    //                 Ok(r) => r,
+    //                 Err(_e) => {
+    //                     tx.send(Err(Error::new(ErrorKind::Other, "xxx2"))).unwrap();
+    //                     return
+    //                 }
+    //             };
+    //             debug!("get client");
+    //             tx.send(Ok(client)).unwrap();
+    //             return
+    //         }
+    //         tx.send(Err(Error::new(ErrorKind::NotFound, "no rows"))).unwrap();
+    //     });
+    // });
+    // th.join().unwrap();
+    // let client = match rx.recv_timeout(Duration::from_millis(100)) {
+    //     Ok(c) => c,
+    //     Err(err) => Err(Error::new(ErrorKind::NotFound, err.to_string()))
+    // };
+    // client
     // let rx = Arc::new(rx);
     // for _i in 0..3 {
     //     debug!("input try_recv");
